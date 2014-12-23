77b152b1ad3f8aef03be9d163bc51720be11933b
iamwave007 <iamwave007@hotmail.com>
2014-12-22 22:38:16 -0500

    half way trhough

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var validationApp = angular.module('validationApp',[]);
+
+validationApp.controller('mainController',function($scope){
+    $scope.submitForm = function(isValid){
+        // check to make sure the form is completely valid
+        if(isValid){
+            alert('our form is amazing');
+        }
+    };
+});
